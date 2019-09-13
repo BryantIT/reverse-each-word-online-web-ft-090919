431e2051924184917326ca8d0328af4078d8a79f
Bryant Richards <dragonseyeimages@gmail.com>
2019-09-13 23:30:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(sentence)
   end
   return sentence1.join(" ")
 end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  sentence1 =[]
+  array.collect do |word|
+    sentence1<< word.reverse
+  end
+  return sentence1.join(" ")
+end
